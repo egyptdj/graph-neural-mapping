a4aaa7d9dbfa85c69584ec7c883b01cf00b9fba4
Byung-Hoon Kim <egyptdj@kaist.ac.kr>
2020-03-25 16:13:10 +0900

    Initial commit

--- models/discriminator.py
+++ models/discriminator.py
@@ -36,34 +36,3 @@ class Discriminator(nn.Module):
         logits = torch.cat((sc_1, sc_2), 0)
 
         return logits
-
-
-#
-# class Discriminator(nn.Module):
-#     def __init__(self, n_h):
-#         super(Discriminator, self).__init__()
-#         self.f_k = nn.Bilinear(n_h, n_h, 1)
-#
-#         for m in self.modules():
-#             self.weights_init(m)
-#
-#     def weights_init(self, m):
-#         if isinstance(m, nn.Bilinear):
-#             torch.nn.init.xavier_uniform_(m.weight.data)
-#             if m.bias is not None:
-#                 m.bias.data.fill_(0.0)
-#
-#     def forward(self, n_f, g_f, n_idx, s_bias1=None, s_bias2=None): #n_f [557,256], g_f [32,256], n_idx [557]
-#         d_criterion = nn.BCEWithLogitsLoss()
-#         loss = 0.0
-#         logit_list = []
-#         for i in range(g_f.shape[0]):
-#             logit = self.f_k(n_f, g_f[i,:].repeat(n_f.shape[0],1)).squeeze()
-#             logit_list.append(logit)
-#             label = (n_idx==i).type_as(logit)
-#             loss += d_criterion(logit, label)
-#         loss /= g_f.shape[0]
-#
-#         logits = torch.stack(logit_list)
-#
-#         return loss, logits
